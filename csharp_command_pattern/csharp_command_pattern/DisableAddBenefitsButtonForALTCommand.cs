a3201ae131f8f87915f9a62d6f175219345a7daa
John Wonson <jmpwonson@gmail.com>
2021-01-13 09:26:52 +1100

    Create DisableAddBenefitsButtonForALTCommand.cs

---
+++ csharp_command_pattern/csharp_command_pattern/DisableAddBenefitsButtonForALTCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace command_design_pattern
+{
+    internal class DisableAddBenefitsButtonForALTCommand : ICommand, ICommandFactory
+    {
+        public string CommandName => "DisableAddBenefitsButtonForALT";
+
+        public void Execute()
+        {
+            Console.WriteLine("\n{0} Command Executed.", CommandName);
+
+            // Implementation of child commands here
+        }
+
+        public ICommand MakeCommand(string arg)
+        {
+            return new DisableAddBenefitsButtonForALTCommand();
+        }
+    }
+}
