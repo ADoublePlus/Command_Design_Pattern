d8a44f0d77d5e08cc2ab7c5b26c9c0fba431b5c4
John Wonson <jmpwonson@gmail.com>
2021-01-13 09:21:38 +1100

    Create DisableAddBenefitsButtonCommand.cs

---
+++ csharp_command_pattern/csharp_command_pattern/DisableAddBenefitsButtonCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace command_design_pattern
+{
+    public class DisableAddBenefitsButtonCommand : ICommand, ICommandFactory
+    {
+        private bool isAlterationsRisk;
+
+        public DisableAddBenefitsButtonCommand(bool isAlterationsRisk)
+        {
+            this.isAlterationsRisk = isAlterationsRisk;
+        }
+
+        public string CommandName => "DisableAddBenefitsButton";
+
+        public void Execute()
+        {
+            Console.WriteLine("\n{0} Command Executed.", CommandName);
+
+            if (isAlterationsRisk) { } // Implementation of child commands here
+        }
+
+        public ICommand MakeCommand(string arg)
+        {
+            return new DisableAddBenefitsButtonCommand(isAlterationsRisk);
+        }
+    }
+}
