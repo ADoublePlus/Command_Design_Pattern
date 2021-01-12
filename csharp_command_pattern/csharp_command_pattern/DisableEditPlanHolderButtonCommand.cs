0b4470dba9562d8eccd266838c8d1cd652b3c08e
John Wonson <jmpwonson@gmail.com>
2021-01-13 09:32:00 +1100

    Create DisableEditPlanHolderButtonCommand.cs

---
+++ csharp_command_pattern/csharp_command_pattern/DisableEditPlanHolderButtonCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace command_design_pattern
+{
+    public class DisableEditPlanHolderButtonCommand : ICommand, ICommandFactory
+    {
+        public string CommandName => "DisableEditPlanHolderButton";
+
+        public void Execute()
+        {
+            Console.WriteLine("\n{0} Command Executed.", CommandName);
+        }
+
+        public ICommand MakeCommand(string arg)
+        {
+            return new DisableEditPlanHolderButtonCommand { };
+        }
+    }
+}
